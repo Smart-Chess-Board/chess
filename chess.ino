1051c168bbec839ac4e1fc334fef1e9691e7825c
Adam Wei <adam.wei@mail.utoronto.ca>
2021-08-16 23:06:26 -0400

    debug test, print chess board to serial monitor

--- chess.ino
+++ chess.ino
@@ -1,9 +1,21 @@
+#include <board.h>
+#include <enum.h>
+#include <square.h>
+
+Board board;
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  board.printSerial();
+  Serial.println();
+  Serial.println();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  board.printSerial();
+  Serial.println();
+  Serial.println();
+  delay(10000);
 }
