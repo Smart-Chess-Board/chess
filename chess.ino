99c621df5ed96a6159fab1260c7bbad584f4702c
Adam Wei <adam.wei@mail.utoronto.ca>
2021-08-20 20:14:07 -0400

    updated ino code to match library changes, moved code into helper functions to simplify main loop code

--- chess.ino
+++ chess.ino
@@ -6,45 +6,42 @@
 #include <state.h>
 #include <utility.h>
 
+//#define LED
+
 Board board;
 Event prevEvent;
 Event currEvent;
 Player turn = white;
 State BLACK_STATE = RED;
 State WHITE_STATE = GREEN;
+bool stateChanged = false;
 PieceType promoSelection = empty;
+
+// function prototypes
+State stateOfCurrPlayer();
+void setupStateLEDs();
+void updateStateLEDs();
+void printBoardState();
+void pollCurrEvent();
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+#ifdef LED
+  setupStateLEDs();
+#endif
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
   // print board
-  Serial.print("----------------------------");
-  Serial.println("----------------------------");
-  board.printSerial();
-
-  // print piece in hand
-  if (prevEvent.action == lift){
-    char c = prevEvent.piece.type;
-    if (WHITE_STATE == PROMO3 || BLACK_STATE == PROMO3) c = promoSelection;
-    if (prevEvent.piece.color == black) c += 32;
-    Serial.println("Piece in hand: " + String(c));
-    Serial.println();
-  }
+  Serial.println("--------------------------------------------------------");
+  printBoardState();
   Serial.println();
 
-  // print info
-  if (turn == white) Serial.println("Turn: white");
-  else if (turn == black) Serial.println("Turn: black");
-  Serial.println("white state: " + stateName(WHITE_STATE));
-  Serial.println("black state: " + stateName(BLACK_STATE));
-  Serial.println();
+  // get next event;
   Serial.println("Enter event:");
-  currEvent = board.pollEvent(prevEvent, turn);
+  pollCurrEvent();
   currEvent.printSerial();
   Serial.println();
 
@@ -54,16 +51,12 @@ void loop() {
   Serial.print("prevEvent: ");
   prevEvent.printSerial();
   Serial.println();
-
-  if (currEvent.promotion != empty){
-    promoSelection = currEvent.promotion;
-  }
   
-  // perform updates
-  if (WHITE_STATE == PROMO3 || BLACK_STATE == PROMO3){
-    currEvent.piece.type = promoSelection;
-  }
+  // update board
   board.update(currEvent);
+
+  // update states
+  State prevState = stateOfCurrPlayer();
   WHITE_STATE = nextState(WHITE_STATE, currEvent, prevEvent);
   BLACK_STATE = nextState(BLACK_STATE, currEvent, prevEvent);
   
@@ -77,9 +70,95 @@ void loop() {
       WHITE_STATE = GREEN;
     }
   }
+#ifdef LED
+  setupStateLEDs();
+#endif
 
-  if (currEvent.isNullEvent == false){ // need better code to decide when to update prevEvent
-    prevEvent = currEvent;
+  // update prevEvent
+  stateChanged = (stateOfCurrPlayer() != prevState);
+  updatePrevEvent(currEvent, prevEvent, stateOfCurrPlayer(), stateChanged);
+
+}
+
+State stateOfCurrPlayer(){
+    if (turn == black) return BLACK_STATE;
+    else return WHITE_STATE;
+}
+
+void setupStateLEDs(){
+  // white
+  pinMode(13, OUTPUT); // green
+  pinMode(12, OUTPUT); // yellow
+  pinMode(11, OUTPUT); // red
+  //black
+  pinMode(6, OUTPUT); // green
+  pinMode(5, OUTPUT); // yellow
+  pinMode(4, OUTPUT); // red
+}
+
+void updateStateLEDs(){
+  // set LED
+  if (WHITE_STATE == GREEN){
+    digitalWrite(13, HIGH);
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+  }
+  else if (WHITE_STATE == RED){
+    digitalWrite(13, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(11, HIGH);
+  }
+  else{
+    digitalWrite(13, LOW);
+    digitalWrite(12, HIGH);
+    digitalWrite(11, LOW);
   }
 
+  if (BLACK_STATE == GREEN){
+    digitalWrite(6, HIGH);
+    digitalWrite(5, LOW);
+    digitalWrite(4, LOW);
+  }
+  else if (BLACK_STATE == RED){
+    digitalWrite(6, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(4, HIGH);
+  }
+  else{
+    digitalWrite(6, LOW);
+    digitalWrite(5, HIGH);
+    digitalWrite(4, LOW);
+  }
+}
+
+void printBoardState(){
+  board.printSerial();
+
+  // print piece in hand
+  if (prevEvent.action == lift){
+    char c = prevEvent.piece.type;
+    if (stateOfCurrPlayer() == PROMO3) c = promoSelection;
+    if (prevEvent.piece.color == black) c += 32;
+    Serial.println("Piece in hand: " + String(c));
+    Serial.println();
+  }
+  Serial.println();
+
+  // print info
+  if (turn == white) Serial.println("Turn: white");
+  else if (turn == black) Serial.println("Turn: black");
+  Serial.println("white state: " + stateName(WHITE_STATE));
+  Serial.println("black state: " + stateName(BLACK_STATE));
+}
+
+void pollCurrEvent(){
+  currEvent = board.pollEvent(stateOfCurrPlayer(), prevEvent, turn);
+  // check if event is promotion
+  if (currEvent.promotion != empty){
+    promoSelection = currEvent.promotion; // change this to piece in hand
+  }
+  // check if even
+  if (stateOfCurrPlayer() == PROMO3){ // move into pollEvent
+    currEvent.piece.type = promoSelection;
+  }
 }
